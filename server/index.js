b7603a1ef7fe580e94c028c17f8dc446f4e24c78
ishu-ig <ishaanguptacse@gmail.com>
2026-09-03 10:16:16 +0530

    New Domain Added

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ var whitelist = [
     'http://localhost:4000',
     'http://localhost:5000',
     'https://my-portfolio-next-vszt.onrender.com',
-    'https://my-portfolio-next-sigma.vercel.app'
+    'https://my-portfolio-next-sigma.vercel.app',
+    'https://www.ishaanportfolio.com'
 ]
 
 var corsOptions = {
